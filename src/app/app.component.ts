6f26be2182178b6e421160e0ab29d2f134ad66e4
Zachary Allen <zacha0dev@gmail.com>
2023-07-27 14:12:31 -0400

    Update Application Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,91 +8,80 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements AfterViewInit {
   title = 'world-map-project';
-  hoveredCountryName: string | null = null; // Variable to store the hovered country name
-  countryInfo: any | null = null; // Variable to store the country information
+  hoveredCountryName: string | null = null;
+  countryInfo: any | null = null;
 
   constructor(private http: HttpClient) {}
 
-  @ViewChild('worldMap', { static: false }) worldMap!: ElementRef<SVGSVGElement>; 
+  @ViewChild('worldMap', { static: false }) worldMap!: ElementRef<SVGSVGElement>;
 
-  ngAfterViewInit() { 
-    this.addEventListenersToPaths(); 
-  } 
+  ngAfterViewInit() {
+    this.addEventListenersToPaths();
+  }
 
-  addEventListenersToPaths() { 
-    const svgElement = this.worldMap.nativeElement; 
-    const pathElements = svgElement.querySelectorAll('path'); 
+  addEventListenersToPaths() {
+    const svgElement = this.worldMap.nativeElement;
+    const pathElements = svgElement.querySelectorAll('path');
     pathElements.forEach((path) => {
       const pathId = path.getAttribute('id');
 
       if (!pathId || pathId === 'null') {
-        // Skip paths without valid IDs
-        return;
+        return; // Skip paths without valid IDs
       }
 
-      path.addEventListener('mouseover', (event) => this.onMouseOver(event)); 
-      path.addEventListener('mouseout', (event) => this.onMouseOut(event)); 
-      path.addEventListener('click', (event) => this.onClick(event)); 
-    }); 
-  } 
+      path.addEventListener('mouseover', (event) => this.onMouseOver(event));
+      path.addEventListener('mouseout', (event) => this.onMouseOut(event));
+      path.addEventListener('click', (event) => this.onClick(event));
+    });
+  }
 
-  onMouseOver(event: MouseEvent) { 
-    // Handle mouse over event here 
-    const target = event.target as SVGPathElement; 
+  onMouseOver(event: MouseEvent) {
+    const target = event.target as SVGPathElement;
     const pathId = target.getAttribute('id');
-    
+
     this.http.get<any[]>(`https://api.worldbank.org/V2/country/${pathId}?format=json`)
     .subscribe(
       (response) => {
-        // Log the full API response to the console
-        console.log('Full API Response:', response);
-
         if (response.length > 1) {
           const countryName = response[1][0].name;
-          this.hoveredCountryName = countryName; // Store the country name for the tooltip
+          this.hoveredCountryName = countryName;
         } else {
-          this.hoveredCountryName = null; // No data found, reset the tooltip
+          this.hoveredCountryName = null;
         }
       },
       (error) => {
         console.error('Error fetching data:', error);
       }
     );
+  }
 
-    // Now you have the path ID and can use it to make API calls or perform other actions 
-  } 
+  onMouseOut(event: MouseEvent) {
+    this.hoveredCountryName = null;
+  }
 
-  onMouseOut(event: MouseEvent) { 
-    this.hoveredCountryName = null; // Reset the tooltip when mouse leaves the path
-  } 
+  onClick(event: MouseEvent) {
+    const target = event.target as SVGPathElement;
+    const pathId = target.getAttribute('id');
 
-  onClick(event: MouseEvent) { 
-    // Handle click event here 
-    const target = event.target as SVGPathElement; 
-    const pathId = target.getAttribute('id'); 
-    
     if (!pathId || pathId === 'null') {
       console.log('Path ID is null, skipping API call.');
       return;
     }
 
-    console.log(pathId + ' clicked'); 
-    // Now you have the path ID and can use it to make API calls or perform other actions 
+    console.log(pathId + ' clicked');
 
-    // Fetch country information and display it in the right column upon clicking
     this.http.get<any[]>(`https://api.worldbank.org/V2/country/${pathId}?format=json`)
     .subscribe(
       (response) => {
         if (response.length > 1) {
-          this.countryInfo = response[1][0]; // Store the full country information for the clicked country
+          this.countryInfo = response[1][0];
         } else {
-          this.countryInfo = null; // No data found, reset the country information
+          this.countryInfo = null;
         }
       },
       (error) => {
         console.error('Error fetching data:', error);
       }
     );
-  } 
-
+  }
 }
